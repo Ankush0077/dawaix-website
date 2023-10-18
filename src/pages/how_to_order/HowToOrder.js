57a44a68ccb72dfb2d7eab87c7e0e6229b649c56
Ankush Yadav <95432146+Ankush0077@users.noreply.github.com>
2023-10-18 13:36:11 +0000

    Create HowToOrder.js

---
+++ src/pages/how_to_order/HowToOrder.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// import components
+import Header from '../../components/Header'
+import HowToOrderVideo from '../../components/HowToOrderVideo'
+
+const HowToOrder = () => {
+  return (
+    <div className="mt-4">
+        <Header />
+        <HowToOrderVideo />
+    </div>
+  )
+}
+
+export default HowToOrder
