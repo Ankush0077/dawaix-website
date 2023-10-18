71aa36e04537d9f89d30aa58eeef0f1fc1c2aa00
Ankush Yadav <95432146+Ankush0077@users.noreply.github.com>
2023-10-18 13:37:39 +0000

    Create OurTeam.js

---
+++ src/pages/our_team/OurTeam.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// import components
+import Header from "../../components/Header"
+import Team from "./components/Team"
+
+const OurTeam = () => {
+  return (
+    <div className="mt-4">
+        <Header />
+        <Team />
+    </div>
+  )
+}
+
+export default OurTeam
